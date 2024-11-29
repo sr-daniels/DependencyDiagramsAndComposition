d4ad0b9ce4243554dfb0d64a9dea9abb8facd460
Sharonda Daniels <sharonda.d27@gmail.com>
2024-11-29 00:00:11 -0800

    Added encapsulation in coordinance with network requirements

--- DependencyDiagramsAndComposition/DependencyExamples.swift
+++ DependencyDiagramsAndComposition/DependencyExamples.swift
@@ -14,19 +14,19 @@ protocol FeedLoader {
 // Solid line, empty head: ---|>,  indicates a "inherits from" / "is a" relationship wheras FeedViewController inherits from UIViewController
 class FeedViewController: UIViewController {
     // Solid line, filled head: ---▶, indicates a "depends on" / "has a" relationship whereas FeedViewController has a FeedLoader
-    var loadFeed: FeedLoader!
+    var loader: FeedLoader!
     
     convenience init(loader: FeedLoader) {
         self.init()
-        self.loadFeed = loadFeed
+        self.loader = loader
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    loader.loadFeed { loadedItems in
-        // Update UI
+        
+        loader.loadFeed { loadedItems in
+            // Update UI
+        }
     }
 }
 
@@ -43,3 +43,27 @@ class LocalFeedLoader: FeedLoader {
         // Do something
     }
 }
+
+struct Reachability {
+    static let networkAvailable = false
+}
+
+class RemoteWithLocalFallbackFeedService: FeedLoader {
+    let remote: RemoteFeedLoader
+    let local: LocalFeedLoader
+    
+    init(remote: RemoteFeedLoader, local: LocalFeedLoader) {
+        self.remote = remote
+        self.local = local
+    }
+    func loadFeed(completion: @escaping ([String]) -> Void) {
+        let load = Reachability.networkAvailable ? remote.loadFeed : local.loadFeed
+        load(completion)
+    }
+}
+
+// Different types of viewmodel initializations
+let vc = FeedViewController(loader: RemoteFeedLoader())
+let vc2 = FeedViewController(loader: LocalFeedLoader())
+let vc3 = FeedViewController()
+vc3.loader = FeedViewController(loader: RemoteWithLocalFallbackFeedService(remote: RemoteFeedLoader(), local: LocalFeedLoader()))
